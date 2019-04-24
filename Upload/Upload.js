60f615d8d78aad3f8422dd642b620c7db58e3356
Leroy <4c65726f79@gmail.com>
2019-04-24 10:50:02 +0200

    wip

--- Upload/Upload.js
+++ Upload/Upload.js
@@ -1,4 +1,7 @@
-alert("Work");
 
 var body = document.getElementsByTagName("BODY")[0];
 body.style.backgroundColor = "red";
+
+var elm = document.getElementsByName("q")
+alert(document.getElementsByName("q").value)
+alert("123")
