5b412a93cb53e3dba23d28a09b2a25835078de8f
Leroy <4c65726f79@gmail.com>
2019-06-05 17:24:50 +0200

    detect solucition side

--- Upload/Upload.js
+++ Upload/Upload.js
@@ -1,7 +1,19 @@
 
 var body = document.getElementsByTagName("BODY")[0];
 body.style.backgroundColor = "red";
+//
+// var elm = document.getElementsByName("q")
+// alert(document.getElementsByName("q").value)
+//
+//
 
-var elm = document.getElementsByName("q")
-alert(document.getElementsByName("q").value)
-alert("123")
+// testen ob auf lösungsseite
+if (typeof document.getElementsByClassName("quizreviewsummary")[0] !== 'undefined') {
+  if (document.getElementsByClassName("quizreviewsummary")[0].getElementsByTagName("tr")[1].getElementsByTagName("td")[0].innerHTML == "Finished" || "Beendet") {
+    //Auf lösungsseite!
+    console.log("finish atemp detect")
+  }
+
+} else {
+  console.log('Nicht auf Lösungsseite');
+}
