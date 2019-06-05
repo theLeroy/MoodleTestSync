16950295c18bcad6d6b203d0601d1ae1a8c100cb
Leroy <4c65726f79@gmail.com>
2019-06-05 18:04:46 +0200

    Filter correct

--- Upload/Upload.js
+++ Upload/Upload.js
@@ -12,8 +12,28 @@ if (typeof document.getElementsByClassName("quizreviewsummary")[0] !== 'undefine
   if (document.getElementsByClassName("quizreviewsummary")[0].getElementsByTagName("tr")[1].getElementsByTagName("td")[0].innerHTML == "Finished" || "Beendet") {
     //Auf lösungsseite!
     console.log("finish atemp detect")
-  }
 
+  //Suche nach richtigen Lösungen:
+    //multichoice!
+    if (document.getElementsByClassName("multichoice")[0] !== 'undefined') {
+      var multichoiceQuestions = document.getElementsByClassName("multichoice");
+      //Loop all multibe choice
+      for (var i = 0; i < (Object.keys(multichoiceQuestions).length)-1; i++) {
+        //Select correct ones
+        if (Object.values(multichoiceQuestions[i].classList).indexOf('correct') > -1) {
+          console.log(multichoiceQuestions[i])
+        }
+      }
+
+
+    }
+
+
+
+
+  } else {
+    alert("noch nicht beendet?? Work in Progress code=aIOg38b3")
+  }
 } else {
   console.log('Nicht auf Lösungsseite');
 }
